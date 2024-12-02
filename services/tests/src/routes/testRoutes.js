5311b49d6c227768e02b9cf48f53334a8ce9cd46
Leomont07 <monteroleon52@gmail.com>
2024-12-02 15:31:34 -0600

    Revert "DB"
    This reverts commit 07267a45a202c68d4a62c69a19791c3462d0637a.

--- services/tests/src/routes/testRoutes.js
+++ services/tests/src/routes/testRoutes.js
@@ -4,7 +4,6 @@ const respuestaController = require('../controllers/respuestaController');
 
 const router = express.Router();
 
-router.post('/', testController.test);
 router.post('/create', testController.createTest);
 router.post('/question', testController.getAdaptiveQuestion);
 router.post('/save', respuestaController.saveResponse);
