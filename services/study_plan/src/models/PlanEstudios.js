a17be0e96ee76ad216ee2e0e5a902d112f27cd38
Leomont07 <monteroleon52@gmail.com>
2024-12-02 12:23:54 -0600

    Plan Estudios

--- services/study_plan/src/models/PlanEstudios.js
+++ services/study_plan/src/models/PlanEstudios.js
@@ -5,11 +5,11 @@ const PlanEstudios = sequelize.define('PlanEstudios', {
     id_plan: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     id_usuario: { type: DataTypes.INTEGER },
     nivel_inicial: { type: DataTypes.STRING },
-    nivel_final: { type: DataTypes.STRING },
+    nivel_destino: { type: DataTypes.STRING },
     fecha_generacion: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
     contenido: { type: DataTypes.TEXT } // JSON con el detalle del plan
 }, {
-    tableName: 'planes_estudios',
+    tableName: 'planes_estudio',
     timestamps: false
 });
 
