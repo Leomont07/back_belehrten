91f25419397ae93ca3ef174d90347c94b02e5cf0
Emaus025 <remiileyva@gmail.com>
2024-12-04 18:08:18 -0600

    retro

--- services/tests/src/routes/testRoutes.js
+++ services/tests/src/routes/testRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const testController = require('../controllers/testController');
 const respuestaController = require('../controllers/respuestaController');
+const retroController = require('../controllers/retroController');
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ router.post('/create', testController.createTest);
 router.post('/question', testController.getAdaptiveQuestion);
 router.post('/save', respuestaController.saveResponse);
 router.post('/finish/:id_test', testController.finishTest);
+router.post('/generateResults/:id_test', retroController.generateResults);
 
 module.exports = router;
